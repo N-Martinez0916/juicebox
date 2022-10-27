5347346b2454fd9e6ab4bdfe0ac28689d3dadecd
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-27 10:31:37 -0400

    cleaned up the code so it works properly

--- db/index.js
+++ db/index.js
@@ -1,66 +1,34 @@
-<<<<<<< HEAD
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
+
 async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username 
-      FROM users;
+  const { rows } = await client.query(
+    `SELECT id, username FROM users;
     `);
-  
+
     return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `, [username, password])
+
+    return rows
+  } catch (error) {
+    throw error
   }
-  
-  // and export them
-  module.exports = {
-    client,
-    getAllUsers,
-  }
+}
+
 
 module.exports = {
   client,
+  getAllUsers,
+  createUser,
 }
-
-client.query(`
-  INSERT INTO users(username, password) VALUES ($1, $2);
-`, [ "some_name", "some_password" ]);
-
-async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-  
-      `);
-  
-      return result
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-        INSERT INTO users(username, password)
-        VALUES ($1, $2);
-      `, [username, password]);
-  
-      return result;
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  // later
-  module.exports = {
-    // add createUser here!
-  }
-=======
-const { Client } = require('pg');
-
-const client = new Client('postgres://localhost:5432/juicebox-dev');
-
-module.exports = {
-    client,
-}
->>>>>>> bb6889920192653b4b563230c08e1de3b39d5833
--- db/seed.js
+++ db/seed.js
@@ -1,126 +1,95 @@
-<<<<<<< HEAD
 
 const {
     client,
-    getAllUsers // new
+    getAllUsers,
+    createUser,
   } = require('./index');
-  
-  async function testDB() {
+
+  async function dropTables() {
     try {
-      client.connect();
-  
-      const users = await getAllUsers();
-      console.log(users);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
-  }
+      console.log('Starting to drop tables...')
 
-async function testDB() {
-  try {
-    // connect the client to the database, finally
-    client.connect();
-
-    // queries are promises, so we can await them
-    const {rows} = await client.query(`SELECT * FROM users;`);
-
-    // for now, logging is a fine way to see what's up
-    console.log(rows);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
-  }
-}
-
-
-async function dropTables() {
-    try {
       await client.query(`
-  
+      DROP TABLE IF EXISTS users;
       `);
+
+      console.log('Finishing dropping tables!')
+
     } catch (error) {
-      throw error; // we pass the error up to the function that calls dropTables
+      console.error('Error dropping tables!')
+      throw error;
     }
   }
-  
-  // this function should call a query which creates all tables for our database 
+
   async function createTables() {
     try {
+      console.log('Starting to build table...')
+
+
       await client.query(`
-  
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar (255) UNIQUE NOT NULL,
+        password varchar (255) NOT NULL
+      );
       `);
+
+
+      console.log('Finished building tables!')
     } catch (error) {
-      throw error; // we pass the error up to the function that calls createTables
+      console.error("Error building tables!")
+      throw error;
     }
   }
-  
+
+  async function createInitialUsers() {
+    try {
+      console.log('Starting to create users...')
+
+      const albert = await createUser({ username: 'albert', password: 'bertie99' })
+
+      const sandra = await createUser({ username: 'sandra', password: 'glamgal' });
+
+      console.log(albert, sandra)
+
+      console.log('Finishing creating users!')
+    } catch (error) {
+      console.error('Error creating users!')
+      throw error
+    }
+  }
+
+
   async function rebuildDB() {
     try {
       client.connect();
-  
+
       await dropTables();
       await createTables();
+      await createInitialUsers();
     } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
+      throw error;
+    }
+  }
+
+  async function testDB() {
+    try {
+      console.log("Starting to test database...");
+  
+      const users = await getAllUsers();
+      console.log("getAllUsers:", users);
+  
+      console.log("Finished database tests!");
+    } catch (error) {
+      console.error("Error testing database!");
+      throw error;
     }
   }
   
-  rebuildDB();
-
-testDB();
-
-async function dropTables() {
-    try {
-      await client.query(`
-        DROP TABLE IF EXISTS users;
-      `);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async function createTables() {
-    try {
-      await client.query(`
-        CREATE TABLE users (
-          id SERIAL PRIMARY KEY,
-          username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
-        );
-      `);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-
+  
   rebuildDB()
-  .then(testDB)
-  .catch(console.error)
-  .finally(() => client.end());rebuildDB()
-=======
-const { client } = require('./index')
-
-async function testDB() {
-    try{
-        client.connect();
-
-        const { rows } = await client.query(`SELECT * FROM users;`);
-
-        console.log(rows);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        client.end();
-    }
-}
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
 
 
-testDB();
->>>>>>> bb6889920192653b4b563230c08e1de3b39d5833
